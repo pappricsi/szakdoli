ad29448606e963d6c3a6fccc8dfc1a5f22b30ae9
Papp Richárd <34957012+pappricsi@users.noreply.github.com>
2020-04-04 14:25:21 +0200

    S

--- Szakdoli/Szakdoli/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
+++ Szakdoli/Szakdoli/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
@@ -3,16 +3,17 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Szakdoli.Models;
 
 namespace Szakdoli.Areas.Identity.Pages.Account.Manage
 {
     public class PersonalDataModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<Alkalmazott> _userManager;
         private readonly ILogger<PersonalDataModel> _logger;
 
         public PersonalDataModel(
-            UserManager<IdentityUser> userManager,
+            UserManager<Alkalmazott> userManager,
             ILogger<PersonalDataModel> logger)
         {
             _userManager = userManager;
