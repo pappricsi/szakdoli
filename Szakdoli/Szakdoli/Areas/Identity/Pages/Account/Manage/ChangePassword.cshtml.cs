444535f1addc11909a51dcf3c27beccb7f9faef9
Papp Richárd <34957012+pappricsi@users.noreply.github.com>
2020-05-11 13:59:22 +0200

    nyelv+ hiba kezelés

--- Szakdoli/Szakdoli/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ Szakdoli/Szakdoli/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -37,18 +37,18 @@ namespace Szakdoli.Areas.Identity.Pages.Account.Manage
         {
             [Required]
             [DataType(DataType.Password)]
-            [Display(Name = "Current password")]
+            [Display(Name = "Jelenlegi jelszó")]
             public string OldPassword { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "New password")]
+            [Display(Name = "Új jelszó")]
             public string NewPassword { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm new password")]
-            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+            [Display(Name = "Új jelszó megerősítése")]
+            [Compare("NewPassword", ErrorMessage = "A két jelszónak meg kell egyezni.")]
             public string ConfirmPassword { get; set; }
         }
 
